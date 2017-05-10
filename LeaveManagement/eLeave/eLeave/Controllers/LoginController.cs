ab2e3bb0f1c944a4a9a4e12f561c2f07bfa5682c
DESKTOP-714C7LA\ISA_SUPPORT <suza.cse035@gmail.com>
2017-05-10 15:52:40 +0600

    css, js and partial layout hasbeen added

---
+++ LeaveManagement/eLeave/eLeave/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eLeave.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            if (Session["login"] == "T")
+            {
+                Response.Write("HOME");
+            }
+            else
+            {
+                Session["login"] = "F";
+            }
+            return View();
+        }
+	}
+}
